12cf600ab750a2310c7963ac4ebebef1e8faad6d
cristianomorceli <86701034+cristianomorceli@users.noreply.github.com>
2021-07-06 22:10:37 -0300

    Correção desafio

--- desafios/desafio1/main.go
+++ desafios/desafio1/main.go
@@ -1,25 +1,47 @@
 package main
 
 import (
-	"html/template"
-	"log"
+	"fmt"
 	"net/http"
 )
 
-var templates *template.Template
-
 func main() {
-
-	templates = template.Must(template.ParseGlob("templates/*.html"))
-
 	http.HandleFunc("/", HomePage)
-	//http.ListenAndServe(":8080", nil)
-
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.ListenAndServe(":8080", nil)
 }
 
 func HomePage(w http.ResponseWriter, r *http.Request) {
-	templates.ExecuteTemplate(w, "home.html", nil)
 
-	//fmt.Fprintf(w, "This is the Home page!")
+	conteudoHtml := `
+	<html>
+    <head>
+        <title>Imersão Full Cycle</title>
+        <style>
+            .titulo{
+                text-align: center;
+                color: red;
+            }
+            body {
+                background-image: url('https://sm.ign.com/ign_br/screenshot/default/marvels-avengers-background-01-ps4-24jan19-en-us_p7hh.jpg');
+                background-position: center;
+                background-repeat: no-repeat;
+  				background-size: contain;
+                background-color: transparent;
+            }
+            p{
+                text-align: justify;
+                color:black;
+                font-size: 12pt;
+            }
+        </style>
+    </head>
+    <body>
+        <div class='titulo'>
+            <h1>Imersão Full Cycle</h1>
+        </div>
+    </body>
+</html>	
+`
+
+	fmt.Fprintf(w, conteudoHtml)
 }
